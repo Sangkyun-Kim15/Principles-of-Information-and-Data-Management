aa0a7e51735f04ae97f3b10708c27f13a7d1eac1
Sangkyun Kim <54552343+Sangkyun-Kim15@users.noreply.github.com>
2021-09-11 18:28:20 -0700

    Update ApplicationDB.java

--- cs336Project/src/com/cs336Project/pkg/ApplicationDB.java
+++ cs336Project/src/com/cs336Project/pkg/ApplicationDB.java
@@ -32,7 +32,7 @@ public class ApplicationDB {
 		}
 		try {
 			//Create a connection to your DB
-			connection = DriverManager.getConnection(connectionUrl,"admin", "adminadmin");
+			connection = DriverManager.getConnection(connectionUrl,"userid", "password");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
